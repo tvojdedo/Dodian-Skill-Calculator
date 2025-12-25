18ef2b5e5d6c494bb5b7e3365080f9e883181159
Acidberry <95976708+Acidberry@users.noreply.github.com>
2025-12-25 05:08:54 +0100

    Fix formatting issues in app.py

--- app.py
+++ app.py
@@ -8,7 +8,6 @@ app = Flask(__name__)
 def index():
     # Tu musíme poslať 'skills' (množné číslo), lebo index.html to tak vyžaduje
     return render_template("index.html", skills=SKILLS)
-
 @app.route("/skill/<skill_name>", methods=["GET", "POST"])
 def skill_detail(skill_name):
     # Skontrolujeme, či skill existuje v našom zozname
